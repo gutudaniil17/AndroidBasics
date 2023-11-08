622c98691ae025c96ce780a09fd31285217e6d85
gutudaniil17 <daniil.gutu@iongroup.com>
2023-11-08 15:36:51 +0200

    Fragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,36 +2,32 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var todoList = mutableListOf(
-            Todo("Volleyball", false),
-            Todo("Volleyball", false),
-            Todo("Volleyball", false),
-            Todo("Volleyball", false),
-            Todo("Volleyball", false),
-            Todo("Volleyball", false)
-        )
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
 
-        val adapter = TodoAdapter(todoList)
-        val rvTodos = findViewById<RecyclerView>(R.id.rvTodos)
-        rvTodos.adapter = adapter
-        rvTodos.layoutManager = LinearLayoutManager(this)
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment).commit()
+        }
 
-        val btnAddTodo = findViewById<Button>(R.id.btnAddTodo)
-        btnAddTodo.setOnClickListener {
-            val title = findViewById<EditText>(R.id.etTodo).text.toString()
-            val todo = Todo(title, false)
-            todoList.add(todo)
-            adapter.notifyItemInserted(todoList.size-1)
+        val btnFragment1 = findViewById<Button>(R.id.btnFragment1)
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment).commit()
+            }
+        }
+
+        val btnFragment2 = findViewById<Button>(R.id.btnFragment2)
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment).commit()
+            }
         }
     }
 }
